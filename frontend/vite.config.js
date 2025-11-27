1b0a39f99b10ce362d8894164ac9c17c00199ca8
adrian-trustap <adrian@trustap.com>
2025-11-27 14:50:36 +0000

    go

--- frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/trustap-demo/', // change to your repo name for GH Pages
-})
